c21cfd76af3da254906bd1e965c94a8999d8521a
zanderASC5 <zanderjenkins@gmail.com>
2018-07-12 11:34:33 -0300

    Function and random practice

---
+++ randy.js
@@ -0,0 +1,46 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const foods = ["pizza","burgers","rice and beans","lasagna","macaroni and cheese","baked potatoes"];
+
+const students = ["Bob","Martin","Danny","Mahdi","Harry","Sean","Robert"];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
+
+
+// function randnum(){
+//     const entay=Math.random();
+//     const pedal= entay*20;
+//     const num= Math.floor(pedal);
+//     console.log(num);
+
+// }
+
+// randnum();
+
+//Maybe try assinging names and letters to numbers 
+
+// function child(){
+//     const SN=Math.random();
+//     const SteN= SN*7;
+//     const final= Math.floor(SteN);
+//     console.log(students[final]);
+
+// }
+
+// child();
+
+function randlet(){
+    
+}
+function ranfod(){
+    const fd= Math.random();
+    const fdk= fd*foods.length
+    const fdkl=Math.floor(fdk)
+    console.log(foods[fdkl])
+}
+
+ranfod();
